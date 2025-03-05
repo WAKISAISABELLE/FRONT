b6739a8a09f4771d444c7047e77453b380e09757
WAKISAISABELLE <wakisaisabelle@gmail.com>
2025-03-05 17:34:09 +0300

    isabelle

--- src/components/login.js
+++ src/components/login.js
@@ -3,26 +3,6 @@ import React from "react";
 import {useState} from "react"
 
 
-export default function Login(){
-    const[email, setEmail]  = useState("");
-    const[password, setPassword] = useState("");
-    const[error, setError] = useState("")
-    const[isLoading, setIsLoading]= useState(false)
-    const handleSubmit = async (e: React.FormEvent)=>{
-        e.preventDefault()
-        setIsLoading(true)
-        setError("")
-
-        try{
-            await new Promise((resolve) => setTimeout(resolve, 1000))
-            if (email.includes("admin")){
-                 .push("/Dashboard/admin")
-            } else if (email.includes("president")){
-                router.push("/Dashboard/president")
-            }else{
-                router.push("/Dashboard/student")
-            }
-        }cat
     return(
         <div className = 'Login-container'>
             <h2> Login</h2>
