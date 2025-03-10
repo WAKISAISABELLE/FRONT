2212154d64a4856ce36f86e36812ab474ca25fd8
WAKISAISABELLE <wakisaisabelle@gmail.com>
2025-03-10 09:31:33 +0300

    .

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Admin from './Dashboard/Admin.js';
 import Student from './Dashboard/Student.js';
+import Welcome from './components/welcome.js'; // Ensure you have this component
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/admin" element={<Admin />} />
         <Route path="/student" element={<Student />} />
-        {/* <Route path="/" element={<Welcome />} /> */}
+        <Route path="/" element={<Welcome />} />
       </Routes>
     </Router>
   );
